1fc5854293fc02ea86d24d7de58b94bdb956898e
AaronTan <aarontys1999@gmail.com>
2023-04-19 01:37:41 +0800

    margin top for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import whatsapp from "../images/WhatsAppButtonGreenMedium.svg"
 
 function LinkCard({href,title,image}) {
   return(
-    <a href={href} className='flex items-center p-1 w-full rounded-md hover:scale-105 transition-all border border-gray-300 mb-3 bg-gray-100 max-w-3xl ' >
+    <a href={href} className='flex items-center p-1 w-full rounded-md hover:scale-105 transition-all border border-gray-300 mb-3 bg-gray-100 max-w-3xl'>
       <div className='flex items-center text-center w-full'>
         <div className='h-10 flex items-center'>
           {image &&(
@@ -50,7 +50,7 @@ export default function Home() {
           <CiShare1 size={25} color={"#9ffd32"}/>
         </a>
       </div>
-      <div className='flex flex-col items-center justify-center mx-auto w-full mt-16 px-8'>
+      <div className='flex flex-col items-center justify-center mx-auto w-full mt-12 md:mt-16 px-8'>
         <Image className='rounded-full'
           src={data.avatar}
           width={96}
